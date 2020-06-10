34eea4e7d4b31a33aca72fe3fe4b16fcc0c6c1ee
nevikw39 <nevik.w39@gmail.com>
2020-06-10 16:16:28 +0800

    recall

--- recall/recall.py
+++ recall/recall.py
@@ -1,3 +1,8 @@
 import sys
-n, a, b = map(int, sys.stdin.buffer.readline().decode().split())
-sys.stdout.buffer.write((("!!666\n%d" if a >= n / 4 and a > b else "QQ~~\n%d") % (n - a - b)).encode())
+
+def main():
+    n, a, b = map(int, sys.stdin.buffer.readline().decode().split())
+    sys.stdout.buffer.write((("!!666\n%d" if a >= n / 4 and a > b else "QQ~~\n%d") % (n - a - b)).encode())
+
+if __name__ == "__main__":
+    main()
