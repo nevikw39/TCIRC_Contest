76979d2317d959b0d84699942e00ec88da24098e
nevikw39 <nevikw39@icloud.com>
2020-06-28 01:31:32 +0800

    recall if-else

--- recall/recall.py
+++ recall/recall.py
@@ -4,7 +4,7 @@ import sys
 def main():
     n, a, b = map(int, sys.stdin.buffer.readline().decode().split())
     sys.stdout.buffer.write(
-        (("!!666\n%d" if a >= n / 4 and a > b else "QQ~~\n%d") % (n - a - b)).encode())
+        (("!!666\n%d" if a << 2 >= n and a > b else "QQ~~\n%d") % (n - a - b)).encode())
 
 
 if __name__ == "__main__":
